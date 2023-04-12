d1f286ca666e1fbd5457f8461be817b930d697f7
breadcheroso <102551644+breadcheroso@users.noreply.github.com>
2023-04-12 19:02:20 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBk9koyVDILmNTyOLVXVGIsmhMmBIq0XsA",
+    authDomain: "espectagrama-19a42.firebaseapp.com",
+    databaseURL: "https://espectagrama-19a42-default-rtdb.firebaseio.com",
+    projectId: "espectagrama-19a42",
+    storageBucket: "espectagrama-19a42.appspot.com",
+    messagingSenderId: "364939187756",
+    appId: "1:364939187756:web:d51a0ecee75e5938d26389"
+  };
